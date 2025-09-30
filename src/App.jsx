4fa704177d4ce0553b43857a537cb4cb9107c719
armamunsyl <armamunsyl@gmail.com>
2025-09-30 22:00:19 +0600

    made top card structuire

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './component/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="navbar bg-base-100  mx-auto max-w-7xl">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">CS — Ticket System</a>
+      <Navbar></Navbar>
+      <div className='flex justify-center mx-auto max-w-7xl gap-6 text-center mt-10'>
+        <div className='relative w-64 h-32 rounded-xl shadow-lg bg-gradient-to-r from-violet-500 to-purple-600 flex flex-col justify-center items-center text-white'>  
+          <h1 className="text-lg font-medium" >in progress</h1>
+          <span className="text-3xl font-bold">0</span>
         </div>
-
-        <div className="navbar-end">
-          <ul className="menu menu-horizontal px-6">
-            <li><a>Home</a></li>
-            <li><a>FAQ</a></li>
-            <li><a>Changelog</a></li>
-            <li><a>Blog</a></li>
-            <li><a>Download</a></li>
-            <li><a>Contact</a></li>
-          </ul>
-          <button className=" btn flex items-center gap-2 px-4 py-2 text-white font-medium rounded bg-gradient-to-r from-purple-600 to-purple-400">
-            <span className="text-lg">+</span>
-            New Ticket
-          </button>
+        <div className='relative w-64 h-32 rounded-xl shadow-lg bg-gradient-to-r from-green-400 to-emerald-600 flex flex-col justify-center items-center text-white'>
+          <h1 className="text-lg font-medium" >in progress</h1>
+          <span className="text-3xl font-bold">0</span>
         </div>
       </div>
     </>
