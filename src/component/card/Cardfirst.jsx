0f0e0575b8135e22ef1d0580a46e0a266b1d024a
armamunsyl <armamunsyl@gmail.com>
2025-10-01 21:13:39 +0600

    added conditional css

--- src/component/card/Cardfirst.jsx
+++ src/component/card/Cardfirst.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Cardfirst() {
     return (
         <div className='flex justify-center mx-auto max-w-7xl gap-6 text-center mt-10'>
-            <div className='relative flex-1 h-40 rounded-xl shadow-lg bg-gradient-to-r from-violet-500 to-purple-600 flex flex-col justify-center items-center text-white'>
+            <div className='relative flex-1 h-50 rounded-xl shadow-lg bg-gradient-to-r from-violet-500 to-purple-600 flex flex-col justify-center items-center text-white'>
                 <h1 className="text-lg font-medium" >in progress</h1>
                 <span className="text-3xl font-bold">0</span>
             </div>
-            <div className='relative flex-1 h-40 rounded-xl shadow-lg bg-gradient-to-r from-green-400 to-emerald-600 flex flex-col justify-center items-center text-white'>
+            <div className='relative flex-1 h-50 rounded-xl shadow-lg bg-gradient-to-r from-green-400 to-emerald-600 flex flex-col justify-center items-center text-white'>
                 <h1 className="text-lg font-medium" >in progress</h1>
                 <span className="text-3xl font-bold">0</span>
             </div>
--- src/component/support-card/SupportCard.jsx
+++ src/component/support-card/SupportCard.jsx
@@ -12,24 +12,24 @@ const SupportCard = ({ UserProm }) => {
         <div className=' grid grid-cols-2 gap-2'>
 
           {
-            userData.map(user => <div className="card w-96 bg-base-100 card-xs shadow-sm mt-1">
+            userData.map(user => <div className="card  bg-base-100 card-xs shadow-sm mt-1">
               <div className="card-body">
                 <div className='flex justify-between'>
-                  <h2 className="card-title">Login Issues - Can't Access Account</h2>
-                  <h2 className='bg-green-300 px-6 pt-1 pb-1 rounded-2xl text-green-900'>Open</h2>
+                  <h2 className="card-title">{user.title}</h2>
+                  <h2 className={`px-6 pt-1 pb-1 rounded-2xl ${user.status === "Open" ? "bg-green-300 text-green-900" : " bg-[#F8F3B9] text-[#9C7700]"}`}>{user.status}</h2>
                 </div>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <p>{user.description}</p>
                 <div className='flex justify-between'>
                   <div className='flex gap-1 text-0.1xl'>
-                    <span>#1001</span>
-                    <h1 className='text-red-500'>HIGH PRIORITY</h1>
+                    <span>{user.id}</span>
+                    <h1 className={ `${user.priority === "HIGH PRIORITY" ? "text-red-500" : user.priority === "LOW PRIORITY" ? "text-green-400" : "text-amber-500"}`}>{user.priority}</h1>
                   </div>
                   <div className='flex gap-1 text-0.1xl'>
                     <h1>
-                      John Smith
+                      {user.customer}
                     </h1>
                     <h1>
-                      1/15/2024
+                      {user.createdAt}
                     </h1>
                   </div>
                 </div>
@@ -47,7 +47,7 @@ const SupportCard = ({ UserProm }) => {
             <h1>Select a ticket to add to Task Status</h1>
           </div>
           <div>
-            <h1>Resolved Task</h1>
+            <h1 className='text-lg font-semibold mb-4'>Resolved Task</h1>
             <h1>No resolved tasks yet.</h1>
           </div>
         </div>
